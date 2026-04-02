7c4e245c620e82686d64e08d7317a56768c2e1b5
rusik18ruslan12-debug <rusik18ruslan12@gmail.com>
2026-04-02 21:13:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+let name = prompt("Введите ваше имя:");
+
+if (name !== null && name !== "") {
+    alert("Привет, " + name + "!");
+} else {
+    alert("Вы не ввели имя");
+}
+
 function doubleNumber() {
     let number = document.getElementById("numberInput").value;
+
+    if (number === "") {
+        console.log("Введите число!");
+        return;
+    }
+
     let result = number * 2;
 
     console.log("Удвоенное число: " + result);
